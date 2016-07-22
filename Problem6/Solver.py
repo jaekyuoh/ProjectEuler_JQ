ce2b78214150e8a22ade96ff556805c31741e2cf
Oh, Jaekyu <joh74@gatech.edu>
2016-07-22 13:20:26 +0900

    Done with Problem6

--- Problem6/Solver.py
+++ Problem6/Solver.py
@@ -9,6 +9,17 @@
 #
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
+def difference():
+    sqr_of_sum = 0
+    sum = 0
+    sum_of_sqr = 0
+    for i in range(1,101):
+        sqr_of_sum += i**2
+        sum += i
+    sum_of_sqr = sum**2
+    return sum_of_sqr-sqr_of_sum
 
 if __name__ == "__main__":
+    result = difference()
+    print result
     print "This is problem6"
---
+++ Problem7/Solver.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+# What is the 10 001st prime number?
+
+
+def ith_prime(num):
+    ith=0
+
+    return ith
+
+if __name__ == "__main__":
+    result = ith_prime(10001)
+    print result
+    print "This is problem6"
