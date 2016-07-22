7d63870f2881babf94f58e83a793da486f70b4b1
Oh, Jaekyu <joh74@gatech.edu>
2016-07-22 11:57:32 +0900

    Done with Problem5

--- Problem5/Solver.py
+++ Problem5/Solver.py
@@ -3,6 +3,22 @@
 #
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-if __name__ == "__main__":
+import sys
+def solver():
+    num_list = range(1, 21)
+    i = len(num_list)
+    while True:
+        count = 0
+        for k in range(1, 21):
+            if (i % k == 0):
+                count += 1
+        if (count == 20):
+            return i
+        i += 1
+    return 0
 
+
+if __name__ == "__main__":
+    result = solver()
+    print result
     print "This is problem5"
